3e88c0a150490cfaaca13c644e0fb32fd853814f
Saiberion <saiberion@web.de>
2012-09-09 19:07:55 +0200

    added 10 seconds delay between broadcast and shutdown

--- src/main/java/de/minestar/autorestart/threads/StopThread.java
+++ src/main/java/de/minestar/autorestart/threads/StopThread.java
@@ -11,6 +11,12 @@ public class StopThread implements Runnable {
 	@Override
 	public void run() {
 		Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[" + AutoRestartCore.NAME + "] Der Server wird jetzt neu gestartet!");
+		// 10 seconds delay to see the broadcast
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			System.out.println("Sleep wurde unterbrochen");
+		}
 		Bukkit.savePlayers();
 		for (World world : Bukkit.getWorlds()){
 			world.save();
