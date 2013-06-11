2cfa9164dcc2aa3ae9c3f40e867b4a8b937be2c9
GeMoschen <x-chris-x@arcor.de>
2013-06-11 10:25:48 +0200

    - print info to all connected players

--- src/main/java/de/minestar/diehard/threads/SaveAllThread.java
+++ src/main/java/de/minestar/diehard/threads/SaveAllThread.java
@@ -3,16 +3,31 @@ package de.minestar.diehard.threads;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import de.minestar.diehard.core.DieHardCore;
+import de.minestar.syncchest.library.PlayerUtils;
 
 public class SaveAllThread implements Runnable {
 
     @Override
     public void run() {
+        Player[] playerList = Bukkit.getOnlinePlayers();
+        for (Player player : playerList) {
+            PlayerUtils.sendMessage(player, ChatColor.DARK_GREEN, DieHardCore.NAME, "Saving playerdata and worlds...");
+        }
+
         Bukkit.getServer().savePlayers();
         List<World> worldList = Bukkit.getWorlds();
         for (World world : worldList) {
             world.save();
+        }
+
+        playerList = Bukkit.getOnlinePlayers();
+        for (Player player : playerList) {
+            PlayerUtils.sendMessage(player, ChatColor.DARK_GREEN, DieHardCore.NAME, "Done!");
         }
     }
 
