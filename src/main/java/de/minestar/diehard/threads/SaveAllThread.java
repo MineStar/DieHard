f1cc3bb441242fc4475c079645e3b9ade8f6a42b
GeMoschen <x-chris-x@arcor.de>
2013-06-11 10:45:37 +0200

    - remove PlayerUtils

--- src/main/java/de/minestar/diehard/threads/SaveAllThread.java
+++ src/main/java/de/minestar/diehard/threads/SaveAllThread.java
@@ -5,30 +5,20 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
 
 import de.minestar.diehard.core.DieHardCore;
-import de.minestar.syncchest.library.PlayerUtils;
 
 public class SaveAllThread implements Runnable {
 
     @Override
     public void run() {
-        Player[] playerList = Bukkit.getOnlinePlayers();
-        for (Player player : playerList) {
-            PlayerUtils.sendMessage(player, ChatColor.DARK_GREEN, DieHardCore.NAME, "Saving playerdata and worlds...");
-        }
-
+        Bukkit.broadcastMessage(ChatColor.AQUA + "[" + DieHardCore.NAME + "] " + ChatColor.DARK_GREEN + "Saving playerdata and worlds...");
         Bukkit.getServer().savePlayers();
         List<World> worldList = Bukkit.getWorlds();
         for (World world : worldList) {
             world.save();
         }
-
-        playerList = Bukkit.getOnlinePlayers();
-        for (Player player : playerList) {
-            PlayerUtils.sendMessage(player, ChatColor.DARK_GREEN, DieHardCore.NAME, "Done!");
-        }
+        Bukkit.broadcastMessage(ChatColor.AQUA + "[" + DieHardCore.NAME + "] " + ChatColor.DARK_GREEN + "Saving playerdata and worlds...");
     }
 
 }
